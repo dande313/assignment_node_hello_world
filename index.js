1ec35960541bd1c46eec64d6fd69b853afa78f09
Allen Anderson <dande313@byu.net>
2017-06-19 14:31:16 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger');
+var logs= require('./data/logs');
 
+/*
 console.log(chalk.red("Hello World!"));
 console.log(chalk.yellow("Hello Alice!"));
 console.log(chalk.green("Hello Bob!"));
 
-var testArr= ["item1", "carrots", "zed"]
+ var testArr= ["item1", "carrots", "zed"]
 _.each(testArr, function(i){
 	console.log(i)
-})
+}); */
+//logger.test();
+
+_.each(logs, function(log){
+	logger.log(log.message, log.level);
+});
+
+
